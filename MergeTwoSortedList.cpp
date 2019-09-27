f12ec3487062d4fdad41b143486a693fe81accef
superchee <ggccqq0526LD@gmail.com>
2019-09-27 14:47:21 +0800

    merge two sorted list

--- MergeTwoSortedList.cpp
+++ MergeTwoSortedList.cpp
@@ -13,6 +13,7 @@ struct ListNode {
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1 || !l2) return !l1? l2:l1;
         ListNode *head = NULL, *cur = NULL;
 
         if (l1->val <= l2->val){
