de2ea6e4f383bf3dbce6fd07162170622e62241a
superchee <ggccqq0526LD@gmail.com>
2019-10-21 15:52:57 +0800

    unique binary search tree

---
+++ UniqueBinarySearchTree.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> G(n+1,0);
+        G[0] = G[1] = 1;
+
+        for (int i = 2; i <= n; i++){
+            for (int j = 0; j < i; j++){
+                G[i] += G[j]*G[i-j-1];
+            }
+        }
+        return G[n];
+    }
+};
